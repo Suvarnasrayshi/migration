a9e1b399f5571bb179834d6e687ce96b6024ffa8
Suvarnasrayshi <suvarnasrayshi.sinha.2024@gmail.com>
2024-05-16 10:51:15 +0530

    k

--- exammigration/server.js
+++ exammigration/server.js
@@ -16,3 +16,43 @@ app.use("/",log);
 sequelize.sync({force:false});
 app.listen(process.env.port || 3003);
 console.log("🚀 Running at Port 3003");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.postbookborrow = async (req, res) => {
+  console.log(req.body);
+  const{book_id,member_id,borrow_date,return_date}=req.body
+  // console.log(borrow_date);
+  let datereturn=sequelize.literal('DATE_ADD(NOW(), INTERVAL 14 DAY)' )
+  console.log("datareturn",datereturn);
+  try {
+    const newUser = await bookborrow.create(req.body);
+    const newuser =await bookborrow.create(
+      {
+        book_id,
+        member_id,
+        borrow_date,
+        return_date:sequelize.literal('DATE_ADD(NOW(), INTERVAL 14 DAY)' )
+      },
+      {raw: true}
+      )
+
+    res.json(newUser);
+    console.log(newUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
